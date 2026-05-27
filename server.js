5fee409e955183b1ccec35086b99c8c79bbac2b5
malaysri19 <ncrkinggaming@gmail.com>
2026-05-27 12:26:37 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 // Initialize Database
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    ssl: { rejectUnauthorized: true },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
